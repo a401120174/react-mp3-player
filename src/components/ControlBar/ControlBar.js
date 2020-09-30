d0e377a4fa8fcf254300b02728e75c1c6f401b21
louisliao0612 <louisliao@eztravel.com.tw>
2020-09-30 16:43:42 +0800

    feat: volume control

--- src/components/ControlBar/ControlBar.js
+++ src/components/ControlBar/ControlBar.js
@@ -1,4 +1,10 @@
-import React, { useState, useContext, useCallback, useEffect, useRef } from "react";
+import React, {
+  useState,
+  useContext,
+  useCallback,
+  useEffect,
+  useRef,
+} from "react";
 import styled from "styled-components";
 
 import ContextStore from "../../context/globalContext";
@@ -6,104 +12,165 @@ import AudioProvider, { useAudio } from "../../context/audioContext";
 import Progress from "../Progress/Progess";
 
 const Wrapper = styled.div`
-   background-color: var(--black2);
-`;
-
-const ProgressBar = styled.div`
-   background-color: var(--black2);
+  background-color: var(--black2);
 `;
 
 const CommandBar = styled.div`
-   background-color: var(--black2);
-   display: flex;
-   justify-content: space-between;
-   color: var(--white1);
-   align-items: center;
-   padding: 10px;
-   .command {
-      width: 300px;
-      display: flex;
-      flex-direction: row;
-      justify-content: space-between;
-      align-items: center;
-   }
+  background-color: var(--black2);
+  display: flex;
+  justify-content: space-between;
+  color: var(--white1);
+  align-items: center;
+  padding: 10px;
+  .command {
+    width: 300px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
 
 const toTimeStr = (num) => {
-   const minute = Math.floor(num / 60);
-   const second = Math.floor(num % 60);
+  const minute = Math.floor(num / 60);
+  const second = Math.floor(num % 60);
 
-   return `${minute < 10 ? "0" : ""}${minute}:${second < 10 ? "0" : ""}${second}`;
+  return `${minute < 10 ? "0" : ""}${minute}:${
+    second < 10 ? "0" : ""
+  }${second}`;
 };
 
 const ControlBar = () => {
-   const {
-      albums,
-      activeAlbum,
-      setActiveAlbum,
-      songs,
-      activeSong,
-      setActiveSong,
-   } = useContext(ContextStore);
-   const { state, controls } = useAudio();
+  const {
+    albums,
+    activeAlbum,
+    setActiveAlbum,
+    songs,
+    activeSong,
+    setActiveSong,
+  } = useContext(ContextStore);
+  const { state, controls } = useAudio();
+  const [volume, setVolume] = useState(100);
 
-   const handleProgressClick = (e) => {
-      const clickX = e.clientX;
-      const eleX = e.currentTarget.getBoundingClientRect().left;
-      const eleWidth = e.currentTarget.offsetWidth;
-      const percent = ((clickX - eleX) * 100) / eleWidth;
-      console.log(percent);
-      controls.setCurrentTime(Math.floor((state.duration / 100) * percent));
-   };
+  const handleProgressClick = (e) => {
+    const clickX = e.clientX;
+    const eleX = e.currentTarget.getBoundingClientRect().left;
+    const eleWidth = e.currentTarget.offsetWidth;
+    const percent = ((clickX - eleX) * 100) / eleWidth;
+    controls.setCurrentTime(Math.floor((state.duration / 100) * percent));
+  };
 
-   return (
-      <Wrapper>
-         <Progress
-            currentTime={state.time}
-            duration={state.duration}
-            onClick={handleProgressClick}
-         />
-         <CommandBar>
-            <div>
-               {toTimeStr(state.time)} / {toTimeStr(state.duration)}
-            </div>
-            <div className="command">
-               <Btn icon="shuffle" />
-               <Btn icon="skip_previous" />
-               <Btn
-                  icon={state.paused ? "play_circle_outline" : "pause_circle_outline"}
-                  size="M"
-                  onClick={() => (state.paused ? controls.play() : controls.pause())}
-               />
-               <Btn icon="skip_next" />
-               <Btn icon="repeat" />
-            </div>
-            <div>00:00/22:00</div>
-            {state.buffered.start}/{state.buffered.end}
-         </CommandBar>
-      </Wrapper>
-   );
+  const handleVolumeClick = (e) => {
+    const clickX = e.clientX;
+    const eleX = e.currentTarget.getBoundingClientRect().left;
+    const eleWidth = e.currentTarget.offsetWidth;
+    const percent = ((clickX - eleX) * 100) / eleWidth;
+    // console.log(percent);
+    setVolume(percent);
+    // controls.setCurrentTime(Math.floor((state.duration / 100) * percent));
+  };
+
+  return (
+    <Wrapper>
+      <Progress
+        currentTime={state.time}
+        duration={state.duration}
+        onClick={handleProgressClick}
+      />
+      <CommandBar>
+        <div>
+          {toTimeStr(state.time)} / {toTimeStr(state.duration)}
+        </div>
+        <div className="command">
+          <Btn icon="shuffle" />
+          <Btn icon="skip_previous" />
+          <Btn
+            icon={state.paused ? "play_circle_outline" : "pause_circle_outline"}
+            size="M"
+            onClick={() => (state.paused ? controls.play() : controls.pause())}
+          />
+          <Btn icon="skip_next" />
+          <Btn icon="repeat" />
+        </div>
+        <Volume percent={volume} onClick={handleVolumeClick} />
+      </CommandBar>
+    </Wrapper>
+  );
+};
+
+const StyledVolume = styled.div`
+  background-color: #282828;
+  height: 5px;
+  border-radius: 4.5px;
+  position: relative;
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  i {
+    display: block;
+  }
+
+  .barWrapper {
+    width: 100px;
+    border-radius: 4.5px;
+    margin-left: 10px;
+    position: relative;
+    .colorBar {
+      width: ${(props) => props.percent}%;
+      height: 5px;
+      border-radius: 4.5px;
+      background-image: linear-gradient(
+        to right,
+        var(--purple1),
+        var(--orange1)
+      );
+    }
+
+    .circle {
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+      background-color: var(--white1);
+      position: absolute;
+      top: -2px;
+      left: ${(props) => props.percent - 10}%;
+    }
+  }
+`;
+
+const Volume = ({ percent, onClick }) => {
+  return (
+    <StyledVolume percent={percent}>
+      <i class="material-icons">volume_up</i>
+      <div className="barWrapper" onClick={onClick}>
+        <div className="colorBar"></div>
+        <div className="circle"></div>
+      </div>
+    </StyledVolume>
+  );
 };
 
 const StyledBtn = styled.button`
-   color: ${(props) => (props.active ? "var(--purple1)" : "var(--white1)")};
-   transition: 0.5s;
+  color: ${(props) => (props.active ? "var(--purple1)" : "var(--white1)")};
+  cursor: pointer;
+  i {
+    font-size: ${(props) => (props.size === "M" ? "42px" : "26px")};
+  }
 
-   i {
-      font-size: ${(props) => (props.size === "M" ? "42px" : "26px")};
-   }
-
-   &:hover {
-      color: var(--purple1);
-   }
+  &:hover {
+    color: var(--purple1);
+    transition: transform 0.1s;
+    transform: scale(1.1);
+  }
 `;
 
 const Btn = ({ active, onClick, icon, size = "S" }) => {
-   return (
-      <StyledBtn onClick={onClick} active={active} size={size}>
-         <i class="material-icons">{icon}</i>
-      </StyledBtn>
-   );
+  return (
+    <StyledBtn onClick={onClick} active={active} size={size}>
+      <i class="material-icons">{icon}</i>
+    </StyledBtn>
+  );
 };
 
 export default ControlBar;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,147 +6,147 @@ import ContextStore from "../../context/globalContext";
 import ControlBar from "../ControlBar/ControlBar";
 
 const Wrapper = styled.div`
-   background-color: var(--black2);
-   height: 100vh;
-   display: flex;
-   flex-direction: column;
-   flex-grow: 1;
+  background-color: var(--black2);
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
 `;
 
 const Content = styled.div`
-   background-color: var(--black2);
-   flex-grow: 1;
-   /* height: 100vh;
+  background-color: var(--black2);
+  flex-grow: 1;
+  /* height: 100vh;
    display: flex;
    flex-direction: column; */
 `;
 
 const Background = styled.div`
-   background-color: green;
-   height: 330px;
-   background-image: url("./img/${(props) => props.img}.jpg");
-   background-size: cover;
-   background-position: center;
-   position: relative;
+  background-color: green;
+  height: 40%;
+  background-image: url("./img/${(props) => props.img}.jpg");
+  background-size: cover;
+  background-position: center;
+  position: relative;
 
-   &::before {
-      content: "";
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background: rgba(22, 22, 22, 0.8);
-      z-index: 1;
-   }
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(22, 22, 22, 0.8);
+    z-index: 1;
+  }
 `;
 
 const PlayList = styled.div`
-   padding: 0 55px;
-   position: relative;
-   z-index: 2;
-   /* background-color: green; */
-   /* height: 330px; */
-   /* height: 100vh;
+  padding: 0 55px;
+  position: relative;
+  z-index: 2;
+  /* background-color: green; */
+  /* height: 330px; */
+  /* height: 100vh;
    display: flex;
    flex-direction: column; */
 `;
 
 const MusicDesc = styled.div`
-   margin: -130px 0 50px;
-   display: flex;
-   .left {
+  margin: -130px 0 50px;
+  display: flex;
+  .left {
+    width: 185px;
+    height: 185px;
+    background-color: red;
+    img {
       width: 185px;
       height: 185px;
-      background-color: red;
-      img {
-         width: 185px;
-         height: 185px;
-      }
-   }
+    }
+  }
 
-   .mid {
-      padding-left: 25px;
-      color: var(--white1);
-      flex-grow: 1;
-      .album {
-         font-size: 40px;
-         margin-bottom: 35px;
-      }
-   }
+  .mid {
+    padding-left: 25px;
+    color: var(--white1);
+    flex-grow: 1;
+    .album {
+      font-size: 40px;
+      margin-bottom: 35px;
+    }
+  }
 `;
 
 const List = styled.ul``;
 
 const StyledItem = styled.li`
-   cursor: pointer;
-   color: var(--white1);
+  cursor: pointer;
+  color: var(--white1);
 
-   padding: 15px 25px;
-   display: flex;
-   background-color: ${(props) =>
-      props.idx % 2 === 1 ? "var(--black2)" : "var(--black3)"};
+  padding: 15px 25px;
+  display: flex;
+  background-color: ${(props) =>
+    props.idx % 2 === 1 ? "var(--black2)" : "var(--black3)"};
 
-   .play {
-      padding: 0 10px;
-   }
-   .name {
-      flex-grow: 1;
-   }
+  .play {
+    padding: 0 10px;
+  }
+  .name {
+    flex-grow: 1;
+  }
 `;
 
 const Item = ({ name, time, idx }) => {
-   return (
-      <StyledItem idx={idx}>
-         <div>{idx + 1}</div>
-         <div className="play">O</div>
-         <div className="name">{name}</div>
-         <div>{time}</div>
-      </StyledItem>
-   );
+  return (
+    <StyledItem idx={idx}>
+      <div>{idx + 1}</div>
+      <div className="play">O</div>
+      <div className="name">{name}</div>
+      <div>{time}</div>
+    </StyledItem>
+  );
 };
 
 const Main = () => {
-   const {
-      albums,
-      activeAlbum,
-      setActiveAlbum,
-      songs,
-      activeSong,
-      setActiveSong,
-   } = useContext(ContextStore);
+  const {
+    albums,
+    activeAlbum,
+    setActiveAlbum,
+    songs,
+    activeSong,
+    setActiveSong,
+  } = useContext(ContextStore);
 
-   const activeSongs = songs[albums[activeAlbum]];
-   const album = albums[activeAlbum];
+  const activeSongs = songs[albums[activeAlbum]];
+  const album = albums[activeAlbum];
 
-   return (
-      <Wrapper>
-         <Content>
-            <Background img={album} />
-            <PlayList>
-               <MusicDesc>
-                  <div className="left">
-                     <img src={`./img/${album}.jpg`}></img>
-                  </div>
-                  <div className="mid">
-                     <div className="album">Youtube Music</div>
-                     <div className="singer">平到</div>
-                  </div>
-                  <div className="right">
-                     <div>+ 新增至我的專輯</div>
-                     <div>6</div>
-                  </div>
-               </MusicDesc>
-               <List>
-                  {activeSongs.map((song, idx) => (
-                     <Item name={song.name} time={song.time} idx={idx} key={idx} />
-                  ))}
-               </List>
-            </PlayList>
-         </Content>
-         <ControlBar />
-      </Wrapper>
-   );
+  return (
+    <Wrapper>
+      <Content>
+        <Background img={album} />
+        <PlayList>
+          <MusicDesc>
+            <div className="left">
+              <img src={`./img/${album}.jpg`}></img>
+            </div>
+            <div className="mid">
+              <div className="album">Youtube Music</div>
+              <div className="singer">平到</div>
+            </div>
+            <div className="right">
+              <div>+ 新增至我的專輯</div>
+              <div>6</div>
+            </div>
+          </MusicDesc>
+          <List>
+            {activeSongs.map((song, idx) => (
+              <Item name={song.name} time={song.time} idx={idx} key={idx} />
+            ))}
+          </List>
+        </PlayList>
+      </Content>
+      <ControlBar />
+    </Wrapper>
+  );
 };
 
 export default Main;
