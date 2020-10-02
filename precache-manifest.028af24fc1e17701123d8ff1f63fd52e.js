6386dc58de2fd524937ddf50cfdf784218f317bd
Louis Liao <39911137+a401120174@users.noreply.github.com>
2020-10-02 19:20:43 +0800

    Updates

---
+++ precache-manifest.028af24fc1e17701123d8ff1f63fd52e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c167f670f25c1a7f3acf21fc28684953",
+    "url": "/react-mp3-player/index.html"
+  },
+  {
+    "revision": "b39ab3d8b2699e15cd09",
+    "url": "/react-mp3-player/static/css/main.e9b7b68e.chunk.css"
+  },
+  {
+    "revision": "f8c4a590d2e906781362",
+    "url": "/react-mp3-player/static/js/2.a9a572bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-mp3-player/static/js/2.a9a572bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39ab3d8b2699e15cd09",
+    "url": "/react-mp3-player/static/js/main.c66b31db.chunk.js"
+  },
+  {
+    "revision": "d135570e2e372fcc07ae",
+    "url": "/react-mp3-player/static/js/runtime-main.9fe38444.js"
+  }
+]);
